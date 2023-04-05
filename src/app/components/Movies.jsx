7201d9b1b44bd8ab1f344df40347b2e844accd4d
mustafadurmaz <mdmz_001@hotmail.com>
2023-04-05 05:55:28 +0300

    Movie details page added

--- src/app/components/Movies.jsx
+++ src/app/components/Movies.jsx
@@ -1,14 +1,30 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Movies = ({ item }) => {
-  console.log(item);
+  const router = useRouter();
+
   return (
-    <div className="min-w-[500px] relative cursor-pointer imgContainer">
-      <Image width={500} height={300} src={`https://image.tmdb.org/t/p/original/${item?.backdrop_path || item?.poster_path}`} />
+    <div
+      onClick={() => {
+        router.push(`/movie/${item?.id}`);
+      }}
+      className="min-w-[500px] relative cursor-pointer imgContainer"
+    >
+      <Image
+        width={500}
+        height={300}
+        src={`https://image.tmdb.org/t/p/original/${item?.backdrop_path || item?.poster_path}`}
+        style={{ objectFit: "cover" }}
+      />
       <div className="absolute bottom-0 p-3 w-full h-full flex flex-col justify-end opacity-0 hover:opacity-100 transition-opacity">
-        <div className="text-2xl font-bold">{item.title}</div>
-        <div>{item.release_date} - {item.vote_average} </div>
+        <div className="text-2xl font-bold">{item?.title}</div>
+        <div>
+          {item?.release_date} - {item?.vote_average}{" "}
+        </div>
       </div>
     </div>
   );
---
+++ src/app/movie/[id]/page.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React from "react";
+
+const getMovie = async (id) => {
+  const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=46a837ced1c1eedb6163fe2f9bc128f8`);
+  return await res.json();
+};
+
+const Page = async ({ params }) => {
+  const id = params.id;
+
+  const movieDetail = await getMovie(id);
+  console.log(movieDetail);
+  return (
+    <div className="relative p-7 min-h-screen">
+      <Image
+        fill
+        src={`https://image.tmdb.org/t/p/original/${movieDetail?.backdrop_path || movieDetail?.poster_path}`}
+        style={{ objectFit: "cover" }}
+      />
+      <div className="absolute">
+        <div className="text-3xl font-bold my-3">{movieDetail?.title}</div>
+        <div className="w-1/2">{movieDetail?.overview}</div>
+        <div>
+          {movieDetail?.release_date} - {movieDetail?.vote_average}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
