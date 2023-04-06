74bc999439e522637f33412d84bb029e27aeb8bb
mustafadurmaz <mdmz_001@hotmail.com>
2023-04-07 00:09:24 +0300

    Loading page created

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,15 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+
 import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 
 import MenuItem from "./MenuItem";
 import ThemeComp from "./ThemeComp";
 
 const Header = () => {
-  const [keyword, setKeyword]=useState("");
-  const router=useRouter();
+  const [keyword, setKeyword] = useState("");
+  const router = useRouter();
   const menu = [
     {
       name: "About",
@@ -20,17 +21,25 @@ const Header = () => {
     },
   ];
 
-  const searchFunc=(e) => {
-    if(e.key==="Enter" && keyword.length>=3){
-      router.push(`/search/${keyword}`)
+  const searchFunc = (e) => {
+    if (e.key === "Enter" && keyword.length >= 3) {
+      router.push(`/search/${keyword}`);
+      setKeyword("");
     }
-  }
-  
+  };
+
   return (
     <div className="flex items-center gap-7 h-20 p-5">
       <div className="bg-amber-600 rounded-lg p-3 text-2xl font-bold">MovieApp</div>
       <div className="flex flex-1 items-center gap-2 border p-3 rounded-lg">
-        <input onKeyDown={searchFunc} onChange={e=>setKeyword(e.target.value)} placeholder="Arama Yapınız" className="flex-1 outline-none bg-transparent" type="text" />
+        <input
+          value={keyword}
+          onKeyDown={searchFunc}
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder="Arama Yapınız"
+          className="flex-1 outline-none bg-transparent"
+          type="text"
+        />
         <BiSearch size={25} />
       </div>
       <ThemeComp />
--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import Movies from "@/app/components/Movies";
+import React from "react";
 
-const Page = ({params}) => {
-    const keyword=params.keyword;
-    console.log(keyword)
+const Page = async ({ params }) => {
+  const keyword = params.keyword;
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=46a837ced1c1eedb6163fe2f9bc128f8&query=${keyword}&language=en-US&include_adult=false`,
+  );
+
+  const data = await res.json();
+
+  console.log(data);
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      {!data?.results ? (
+        <div>Aranılan film bulunamadı</div>
+      ) : (
+        <div className="flex items-center flex-wrap gap-3">
+          {data?.results?.map((item, index) => (
+            <Movies key={index} item={item} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Page
+export default Page;
